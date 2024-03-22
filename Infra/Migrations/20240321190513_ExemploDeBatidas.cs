824c93357305ea421270f50f7e53aa9c1337a4f0
Christian <christiandmelo@gmail.com>
2024-03-22 15:27:33 -0300

    batidas aleatorias para o mes de fevereiro

--- Infra/Migrations/20240321190513_ExemploDeBatidas.cs
+++ Infra/Migrations/20240321190513_ExemploDeBatidas.cs
@@ -13,6 +13,7 @@ namespace Infra.Migrations
             var almoco = new DateTime(2024, 3, 21, 12, 0, 0, DateTimeKind.Utc);
             var retorno = new DateTime(2024, 3, 21, 13, 0, 0, DateTimeKind.Utc);
             var saida = new DateTime(2024, 3, 21, 18, 0, 0, DateTimeKind.Utc);
+            var baseDate = new DateTime(2024, 2, 1, 0, 0, 0, DateTimeKind.Utc); // Iniciando em 1 de fevereiro de 2024, marcado como UTC
             var random = new Random();
 
             migrationBuilder.InsertData(
@@ -38,6 +39,37 @@ namespace Infra.Migrations
                 columns: new[] { "Id", "DataHora", "TipoPonto", "Observacao", "UserId" },
                 values: new object[] { Guid.NewGuid(), saida, 3, null, userId }
             );
+
+            //Batidas aleatórias para o mês de fevereiro de 2024
+            for (int day = 0; day < 22; day++)
+            {
+                var currentDate = baseDate.AddDays(day);
+
+                // Certifique-se de que cada DateTime esteja especificado como UTC
+                migrationBuilder.InsertData(
+                    table: "Pontos",
+                    columns: new[] { "Id", "DataHora", "TipoPonto", "Observacao", "UserId" },
+                    values: new object[] { Guid.NewGuid(), DateTime.SpecifyKind(currentDate.AddHours(9).AddMinutes(random.Next(60)), DateTimeKind.Utc), 0, null, userId }
+                );
+
+                migrationBuilder.InsertData(
+                    table: "Pontos",
+                    columns: new[] { "Id", "DataHora", "TipoPonto", "Observacao", "UserId" },
+                    values: new object[] { Guid.NewGuid(), DateTime.SpecifyKind(currentDate.AddHours(12).AddMinutes(random.Next(60)), DateTimeKind.Utc), 1, null, userId }
+                );
+
+                migrationBuilder.InsertData(
+                    table: "Pontos",
+                    columns: new[] { "Id", "DataHora", "TipoPonto", "Observacao", "UserId" },
+                    values: new object[] { Guid.NewGuid(), DateTime.SpecifyKind(currentDate.AddHours(13).AddMinutes(random.Next(60)), DateTimeKind.Utc), 2, null, userId }
+                );
+
+                migrationBuilder.InsertData(
+                    table: "Pontos",
+                    columns: new[] { "Id", "DataHora", "TipoPonto", "Observacao", "UserId" },
+                    values: new object[] { Guid.NewGuid(), DateTime.SpecifyKind(currentDate.AddHours(18).AddMinutes(random.Next(60)), DateTimeKind.Utc), 3, null, userId }
+                );
+            }
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
